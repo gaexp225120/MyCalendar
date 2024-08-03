541440b959d312332cfc0248c2f8e6c5f3f01ae3
gaexp225120 <37056487+gaexp225120@users.noreply.github.com>
2024-08-03 18:49:13 +0800

    update structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Todo from "@/components/Todo";
 import TodoCard from "@/components/TodoCard";
 
 export default function Home() {
+  const todoCard = {
+    title: "Todo items",
+  };
   return (
-    <main>
-      <Todo />
-      <TodoCard />
+    <main className="min-h-auto bg-background">
+      <div id="borad-canva" className="mt-3 mx-3">
+        <TodoCard todoProp={todoCard} />
+      </div>
     </main>
   );
 }
