2c804bf0edff8caa166a0f245befede2f4852687
gaexp225120 <37056487+gaexp225120@users.noreply.github.com>
2024-08-03 18:49:26 +0800

    add todo card component

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Card,
   CardContent,
@@ -6,19 +7,40 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useState } from "react";
+import AddTodo from "./AddTodo";
 
-const TodoCard = () => {
+type CardProps = React.ComponentProps<typeof Card>;
+
+type TodoCardType = {
+  title: string;
+  // Add other fields as necessary
+};
+
+type TodoCardProps = CardProps & {
+  todoProp: TodoCardType;
+};
+
+const TodoCard = ({ todoProp }: TodoCardProps) => {
+  const [todoList, setTodoList] = useState<string[]>([]);
+  const { title } = todoProp;
+  const addTodoProps = { setTodoList };
   return (
-    <Card>
+    <Card className="w-[500px] bg-cardbg text-blue-100">
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardTitle className="py-[12px] px-[6px]">{title}</CardTitle>
       </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
+      <CardContent className="grid gap-4">
+        <div className=" flex items-center p-4 rounded-md bg-gray-600 ">
+          <div className="flex-1 space-y-1">
+            <p className="text-sm font-medium leading-none">
+              Push Notifications
+            </p>
+          </div>
+        </div>
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        <AddTodo addTodoProps={addTodoProps} />
       </CardFooter>
     </Card>
   );
